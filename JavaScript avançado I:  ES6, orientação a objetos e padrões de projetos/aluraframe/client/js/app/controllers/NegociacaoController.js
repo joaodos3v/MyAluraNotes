227c1c92e164ed421023171fca9d7a0e6d9d2eff
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-01-14 21:06:31 -0200

    (JavaScript avançado I: ES6, orientação a objetos e padrões de projetos)  Temos o modelo, mas e a view? : Construindo um template dinâmico com a função map

--- JavaScript avançado I:  ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript avançado I:  ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -9,7 +9,7 @@ class NegociacaoController {
 		this._listaNegociacoes = new ListaNegociacoes();
 		this._negociacoesView  = new NegociacoesView($("#negociacoesView"));
 		
-		this._negociacoesView.update();
+		this._negociacoesView.update(this._listaNegociacoes);
 	}
 
 
@@ -17,9 +17,8 @@ class NegociacaoController {
 		event.preventDefault();
 
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
+		this._negociacoesView.update(this._listaNegociacoes);
 		this._limpaFormulario();
-		this._listaNegociacoes.negociacoes.length = 0;
-		console.log(this._listaNegociacoes.negociacoes);
 	}
 
 
--- JavaScript avançado I:  ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/views/NegociacoesView.js
+++ JavaScript avançado I:  ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/views/NegociacoesView.js
@@ -5,7 +5,7 @@ class NegociacoesView {
 	}
 
 
-	_template() {
+	_template(model) {
 		return `
 			<table class="table table-hover table-bordered">
 		        <thead>
@@ -18,6 +18,16 @@ class NegociacoesView {
 		        </thead>
 		        
 		        <tbody>
+		        	${model.negociacoes.map(n => {
+		        		return `
+		        			<tr>
+		        				<td>${DateHelper.dataParaTexto(n.data)}</td>
+		        				<td>${n.quantidade}</td>
+		        				<td>${n.valor}</td>
+		        				<td>${n.volume}</td>
+		        			</tr>
+		        		`
+		        	}).join('')}
 		        </tbody>
 		        
 		        <tfoot>
@@ -27,8 +37,8 @@ class NegociacoesView {
 	}
 
 
-	update() {
-		this._elemento.innerHTML = this._template();
+	update(model) {
+		this._elemento.innerHTML = this._template(model);
 	}
 
 }
