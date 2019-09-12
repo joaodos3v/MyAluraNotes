c7298fa2b0c36df10c7b30b55dca53f6cd72d348
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-09-12 16:04:52 -0300

    (Introdução ao PHP: Primeiros passos com a linguagem) 05: 04

---
+++ Introdução ao PHP: Primeiros passos com a linguagem/php-primeiros-passos/repeticao2.php
@@ -0,0 +1,8 @@
+<?php
+
+// Possíveis evoluções do arquivo 'repeticao.php'
+
+// É recomendado utilizar o 'for' quando, no início da instrução, sabe-se o limite de execuções daquele bloco de código.
+for ($i=1; $i <= 15; $i++) { 
+    echo "#$i" . PHP_EOL;
+}
