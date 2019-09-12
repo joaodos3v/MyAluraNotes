64cfc941aeaf9e8a27cfb9d58f51a327ae009829
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-09-12 14:56:25 -0300

    (Introdução ao PHP: Primeiros passos com a linguagem) 02: 06

---
+++ Introdução ao PHP: Primeiros passos com a linguagem/php-primeiros-passos/tipos.php
@@ -0,0 +1,24 @@
+<?php
+
+/** O PHP é uma Linguagem Dinamicamente Tipada */
+
+
+/**
+ * Tipos Primitivos: [integer, double, string, boolean]
+ */
+
+$idade = 21;
+// echo gettype($idade); // integer
+
+$salario = 1000.30;
+// echo gettype($salario); // double
+
+$divisao = 10 / 3;
+// echo $divisao;
+
+$texto = "Olá Mundo";
+// echo gettype($texto); // string
+
+$verdadeiro = true;
+$falso = false;
+echo gettype($verdadeiro); // boolean
