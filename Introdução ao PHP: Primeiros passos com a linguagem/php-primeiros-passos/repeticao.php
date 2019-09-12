da92217029d4d7824f525cc3be36c8082f04788f
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2019-09-12 15:59:06 -0300

    (Introdução ao PHP: Primeiros passos com a linguagem) 05: 02

---
+++ Introdução ao PHP: Primeiros passos com a linguagem/php-primeiros-passos/repeticao.php
@@ -0,0 +1,9 @@
+<?php
+
+$contador = 1;
+
+// "Se você está copiando e colando código, tem alguma coisa errada. A gente nunca deve precisar fazer isso." - DIAS, Vinicius
+while ($contador <= 15) {
+    echo "#$contador" . PHP_EOL;
+    $contador++;
+}
